8c5f8e7501f5567f38a1e03f7cc959c042d9022b
Bandara K.M.A.P. <97865989+it20640484@users.noreply.github.com>
2024-12-17 22:36:28 +0530

    Update googleSheet.js

--- googleSheet.js
+++ googleSheet.js
@@ -1,18 +1,3 @@
-/*const scriptURL = 'https://script.google.com/macros/s/AKfycbyFFqz8BTedXHF6_JaHUSliFUG0zzGO2sUlT0bAg13vq0Po0nmGCx9qucyTsfpb1qpM/exec'
-
-const form = document.forms['contact-form']
-
-form.addEventListner('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { 
-        method: 'POST', 
-        body: new FormData(form)
-    })
-    .then(response => alert("Thank you! Your message sent successfully."))
-    .then(() => { window.location.reload(); })
-    .catch(error => console.error('Error!', error.message))
-})*/
-
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzXej8tOITgH8B4UcAsLJriP9tQL3-R1pZnQZhMyX9b1KuVhsGGgQYwvLAu5U9IftmfxQ/exec'
 const form = document.forms['contact-form']
 
